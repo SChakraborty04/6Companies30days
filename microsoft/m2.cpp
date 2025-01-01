f7f4266c0177787da0ad3afbe86d868cb489e892
SChakraborty04 <shaanch04@yahoo.com>
2025-01-01 23:42:09 +0530

    solved m2

---
+++ microsoft/m2.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/find-the-winner-of-the-circular-game/
+// Microsoft 2
+class Solution {
+public:
+    int solve(int n,int k){
+        if(n==1) return 0;
+        return (solve(n-1,k)+k)%n;
+    }
+    int findTheWinner(int n, int k) {
+        return solve(n,k)+1;
+    }
+};
